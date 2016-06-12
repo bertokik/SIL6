36c3ea688fabaae214ed5e41fb17568119372fef
FloSIL06 <floriandc38380@gmail.com>
2016-06-12 16:50:19 +0200

    co

--- src/java/jeu/ClientJeuWeb.java
+++ src/java/jeu/ClientJeuWeb.java
@@ -15,22 +15,22 @@ import javax.xml.transform.stream.StreamSource;
  * @author Kik
  */
 public class ClientJeuWeb {
-
+    
     private static WebTarget serviceJeu = null;
-
+    
     public static void main(String args[]) throws Exception {
 
         /*
        ** Initialisation du stub pour interagir avec le service web REST
          */
         serviceJeu = ClientBuilder.newClient().target("http://localhost:8080/Projet_SIL06");
-
+        
         boolean jeuConnexion = true;
         boolean connect = false;
-         
+        Joueur joueur = null;
+
 //        Interface de connexion
         while (jeuConnexion) {
-            
             
             System.out.println("----------------------------------");
             System.out.println("|           Bonjour              |");
@@ -42,7 +42,7 @@ public class ClientJeuWeb {
             System.out.print("Entrez une réponse : ");
             Scanner sc = new Scanner(System.in);
             String rep = sc.nextLine();
-
+            
             switch (rep) {
                 case "1":
                     connect = true;
@@ -56,15 +56,15 @@ public class ClientJeuWeb {
                 default:
                     System.out.println("Ceci n'est pas une réponse valable");
             }
-
+            
             if (connect) {
                 System.out.print("Entrez votre pseudo : ");
                 String pseudo = sc.nextLine();
                 System.out.print("Entrez votre mot de passe : ");
                 String mdp = sc.nextLine();
-
-                Joueur joueur = new Joueur(pseudo, mdp);
-
+                
+                joueur = new Joueur(pseudo, mdp);
+                
                 joueur = inscription(joueur);
                 if (joueur.isConnecte()) {
                     System.out.println("Vous êtes bien connecté");
@@ -72,30 +72,53 @@ public class ClientJeuWeb {
                 connect = joueur.isConnecte();
                 jeuConnexion = true;
             }
-            
-            while (connect) {                
+//            Interface d'accueil
+            while (connect) {
+                System.out.println("");
+                System.out.println("Bonjour " + joueur.getPseudo() + "      Score : " + joueur.getScore());
+                System.out.println("-----------------------------------------");
+                System.out.println("|       1- Voir les parties disponibles |");
+                System.out.println("|       2- Voir les joueurs connectés   |");
+                System.out.println("|       3- Se déconnecter               |");
+                System.out.println("-----------------------------------------");
+                System.out.print("Entrez une réponse : ");
+                rep = sc.nextLine();
                 
+                switch (rep) {
+                    case "1":
+                        
+                        break;
+                    case "2":
+                        
+                        break;
+                    case "3":
+                        connect = false;
+                        joueur.setConnecte(connect);
+                        break;
+                    default:
+                        System.out.println("Ceci n'est pas une réponse valable");
+                }                
             }
         }
-
+        
     }
-
+    
     private static Joueur inscription(Joueur joueur) throws Exception {
         String reponse;
         StringBuffer xmlStr;
         JAXBContext context;
         JAXBElement<Joueur> root;
         Unmarshaller unmarshaller;
-
+        
         context = JAXBContext.newInstance(Joueur.class);
         unmarshaller = context.createUnmarshaller();
-
+        
         reponse = serviceJeu.path("inscription").request().put(Entity.xml(joueur)).readEntity(String.class);
-
+        
         xmlStr = new StringBuffer(reponse);
         root = unmarshaller.unmarshal(new StreamSource(new StringReader(xmlStr.toString())), Joueur.class);
-
+        
         return root.getValue();
     }
-
+    
 }
